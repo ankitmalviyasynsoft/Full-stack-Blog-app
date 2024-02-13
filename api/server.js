14f0d5b5cbb6986541881ab4f640fc7d7865ac3d
Saeed <>
2024-02-13 16:22:13 +0530

    deploy changes

--- api/server.js
+++ api/server.js
@@ -3,6 +3,10 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
