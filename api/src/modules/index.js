865451c06736fe3cdcc585829379e560358d1a23
Saeed <>
2024-02-14 12:29:54 +0530

    some changes

--- api/src/modules/index.js
+++ api/src/modules/index.js
@@ -10,14 +10,11 @@ import uploadsRoutes from './uploadFiles/uploadsRoutes.js';
 const router = express.Router();
 
 
-router.use('/postdummy', (req, res) => {
-  res.send('Hey this is my API postdummy 🥳')
-});
-// router.use('/post', postRoutes);
-// router.use('/files', uploadsRoutes);
-// router.use('/users', usersRoutes);
-// router.use('/banner', bannersRoutes);
-// router.use('/category', categoriesRoutes);
+router.use('/post', postRoutes);
+router.use('/files', uploadsRoutes);
+router.use('/users', usersRoutes);
+router.use('/banner', bannersRoutes);
+router.use('/category', categoriesRoutes);
 
 
 
